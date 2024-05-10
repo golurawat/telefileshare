5c9a5cf3def031e0688f5f40e384d24f9c76abcf
golurawat <negiamangolu04@gmail.com>
2024-05-10 09:05:00 +0530

    fix Timpout 16

--- plugins/start.py
+++ plugins/start.py
@@ -70,7 +70,7 @@ async def start_command(client: Client, message: Message):
         for msg in messages:
 
             if bool(CUSTOM_CAPTION) & bool(msg.document):
-                caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = "Please Forward the File to yourself, It will be disappeared from here after 5 Minutes !!")
+                caption = CUSTOM_CAPTION.format(previouscaption = "Please Forward the File to yourself, It will be disappeared from here after 5 Minutes !!" if not msg.caption else msg.caption.html, filename = "Please Forward the File to yourself, It will be disappeared from here after 5 Minutes !!")
             else:
                 caption = "" if not msg.caption else msg.caption.html
 
